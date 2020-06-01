dc2508aae5f5c7d4761a6151bde85d2653589437
Ivan Tammaro <ivantammaro@TAMMARO-Ivan-iMac27.local>
2020-06-01 22:51:48 +0200

    bp

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -5,20 +5,31 @@
     class="map"
     :zoom="2"
     @click="onClick"
-  />
+  >
+    <MglGeojsonLayer
+      sourceId="maine"
+      :source="geoJsonSource"
+      layerId="maine"
+      :layer="geoJsonLayer"
+    />
+  </MglMap>
 </template>
 
 <script>
 import * as wc from "which-country";
 import Mapbox from "mapbox-gl";
-import { MglMap } from "vue-mapbox";
+import { MglMap, MglGeojsonLayer } from "vue-mapbox";
+import countries from "which-country/lib/data.geo.json";
 
 export default {
   components: {
     MglMap,
+    MglGeojsonLayer,
   },
   methods: {
     onClick(e) {
+      console.log(countries);
+
       const countryId = wc([
         e.mapboxEvent.lngLat.lng,
         e.mapboxEvent.lngLat.lat,
@@ -34,9 +45,30 @@ export default {
     return {
       accessToken: process.env.VUE_APP_MAP_TOKEN, // your access token. Needed if you using Mapbox maps
       mapStyle: "mapbox://styles/tammaroivan/ckawnwq9d0dee1ilnxrnegehr", // your map style
+      test: countries.features,
+      geoJsonSource: {
+        type: "vector",
+        url: "mapbox://saurabhp.countries_tileset",
+      },
+      geoJsonLayer: {
+        "source-layer": "countries",
+        id: "maine",
+        type: "fill",
+        layout: {},
+        paint: {
+          "fill-color": {
+            property: "name",
+            type: "categorical",
+            stops: [
+              ["Argentina", "#A30059"],
+            ],
+          },
+          "fill-opacity": 0.1,
+        },
+      },
     };
   },
-  created() {    
+  created() {
     this.mapbox = Mapbox;
   },
 };
